2e82bd31c69251c357897bf30bb5f6ebd6ee2eb3
Yusuf Can Akyol <canyusufakyol@gmail.com>
2024-08-16 18:46:04 +0300

    error handled

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def get_pokemon_info(pokemon_name):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
     response = requests.get(url)
 
-    if response.status_code == 2g00:
+    if response.status_code == 200:
         pokemon_data = response.json()
         pokemon_info = {
             "name": pokemon_data["name"],
